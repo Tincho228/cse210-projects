f4e165ee5a3e6d9d5108c5893c17b78b62b70521
Tincho228 <martin_quintero521@hotmail.com>
2023-09-20 12:31:58 -0300

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        string response = "yes";
+        while (response == "yes"){
+            Random rnd = new Random();
+            int magic_number = rnd.Next(1,100);
+            int number = -1 ;
+            int guesses = 0;
+            while (magic_number !=  number)
+            {
+                guesses +=1;
+                Console.Write("What is the magic number: ");
+                string user_input = Console.ReadLine();
+                number = int.Parse(user_input);
+                if (number > magic_number){
+                    Console.WriteLine("Lower");
+                }
+                if (number < magic_number){
+                    Console.WriteLine("Higher");
+                }
+            }
+            Console.WriteLine($"You guessed it!, You  made {guesses} guesses");
+            Console.Write("Do you want to play again (yes/no)?");
+            response = Console.ReadLine();
+        }
     }
 }
