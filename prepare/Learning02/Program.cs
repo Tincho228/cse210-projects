60173cc34b1fdd2a8f1cce8784002b216ae3004d
Tincho228 <martin_quintero521@hotmail.com>
2023-09-27 22:12:10 -0300

    learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Google";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2005;
+        job1._endYear = 2007;
+
+        Job job2 = new Job();
+        job2._company = "Microsoft";
+        job2._jobTitle = "Data entry";
+        job2._startYear = 2007;
+        job2._endYear = 2023;
+
+        Resume resume = new Resume();
+        resume._name = "Martin Quintero";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.DisplayResume();
+        
     }
 }
