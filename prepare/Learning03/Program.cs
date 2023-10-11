5e63a3e8477831ac392cfb1cae9ea41220d631ea
Tincho228 <martin_quintero521@hotmail.com>
2023-10-11 13:18:03 -0300

    learning03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+        Fraction myFraction = new Fraction();
+        Fraction wholeFraction = new Fraction(6);
+        Fraction twoFraction = new Fraction(3,4);
+        //Getters
+        Console.WriteLine(myFraction.GetTop());
+        Console.WriteLine(myFraction.GetBottom());
+        Console.WriteLine(wholeFraction.GetTop());
+        Console.WriteLine(wholeFraction.GetBottom());
+        Console.WriteLine(twoFraction.GetTop());
+        Console.WriteLine(twoFraction.GetBottom());
+
+        // Setters
+        wholeFraction.SetTop(5);
+
+        twoFraction.SetTop(6);
+        twoFraction.SetBottom(5);
+        
+        // Output
+        Console.WriteLine(myFraction.GetFractionString());
+        Console.WriteLine(myFraction.GetDecimalValue());
+        Console.WriteLine(wholeFraction.GetFractionString());
+        Console.WriteLine(wholeFraction.GetDecimalValue());
+        Console.WriteLine(twoFraction.GetFractionString());
+        Console.WriteLine(twoFraction.GetDecimalValue());
     }
 }
