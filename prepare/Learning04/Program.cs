c659ce988a5d0b3779a8f76b6744bc782d9e3e10
Tincho228 <martin_quintero521@hotmail.com>
2023-10-26 12:22:38 -0300

    Learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+        
+        MathAssignment mathAssignment = new MathAssignment("Martin Quintero","Multiplication","7.5","8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        WritingAssignment writingAssignment = new WritingAssignment("Martin Quintero","Classic Art","The world of art");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
